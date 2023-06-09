e411aafdc9cc6c79907941286f2237f5dedcca8a
S. Dumas <me@sjdumas.comm>
2023-06-08 20:02:07 -0400

    Added new JS lessons

---
+++ 05-variables/demo.js
@@ -0,0 +1,18 @@
+// Using the var keyword
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+// Using the let keyword
+if (true) {
+    let world = "Hello World";
+
+    console.log(world);
+}
+
+// Using the const keyword
+const aaron = "Aaron";
+
+console.log(aaron);
